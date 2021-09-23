a8689c9deb8573e39c6ce67330f2fd3a2e6765ae
Volodymyr <lotoshead@gmail.com>
2021-09-23 14:36:58 +0300

    new update

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,15 +1,8 @@
 const input = document.querySelector('#font-size-control');
 const span = document.querySelector('#text');
 
-//input.addEventListener('input',action)
-
 function changeSize() {
-  span.style.fontSize = `${input.value}px`;
-}
-
-input.value = parseInt(
-  window.getComputedStyle(document.body).getPropertyValue('font-size')
-);
+    span.setAttribute('style', `font-size: ${input.value}px`);
+};
 
 input.addEventListener('input', changeSize);
-
